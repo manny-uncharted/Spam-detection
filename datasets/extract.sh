1bef68b1cc228171be10f26f2361ee94ac8bb604
manny-uncharted <omoebun52@gmail.com>
2023-07-27 22:30:22 +0100

    Edited dockerfile to include permissions to run entrypoint.sh

---
+++ datasets/extract.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+unzip -o /mnt/c/Users/manny-uncharted/Documents/coding-projects/ai-as-api/datasets/zips/sms-spam-dataset.zip -d /mnt/c/Users/manny-uncharted/Documents/coding-projects/ai-as-api/datasets/spam-classifier/
+
+unzip -o /mnt/c/Users/manny-uncharted/Documents/coding-projects/ai-as-api/datasets/zips/youtube-spam-dataset.zip -d /mnt/c/Users/manny-uncharted/Documents/coding-projects/ai-as-api/datasets/spam-classifier/
